39ee962be54f55c5d7a0315000dd0f36b7ea6f0b
briansleonel <gonzalezbrianleonel76@gmail.com>
2021-06-18 12:48:04 -0300

    se mejora la búsqueda por filtros en el controller de Cuota

--- controllers/cuota.controller.js
+++ controllers/cuota.controller.js
@@ -3,36 +3,27 @@ const Alumno = require('../models/alumno')
 const cuotaCtrl = {};
 
 cuotaCtrl.getCuotas = async (req, res) => {
-  //let criterios = { $or: [{ modo_pago: {$regex : 'merc', $options: 'i'} }, { monto: {$gt:2000}}] };
-
+  //console.log(req.query)
   let criterios = {}
-  let criteriosAlu = {}
-
-  //variables para hacer pruebas
-  modo_pago = '';
-  monto = '';
-
-  apellido = '';
-  nombre = '';
 
   //búsqueda por medio de pago, en modo regex
-  if(modo_pago != '') 
-    criterios.modo_pago = {$regex : modo_pago, $options: 'i'}
+  if(req.query.modo_pago != '') 
+    criterios.modo_pago = {$regex : req.query.modo_pago, $options: 'i'}
   //búsqueda por el monto menor o igual al ingresado
-  if(monto != '')
-    criterios.monto = {$lte: monto };
+  if(req.query.monto != '0')
+    criterios.monto = {$lte: Number(req.query.monto) };
 
   /*
   ----------- Búsquedas por APellido o Nombre de un almuno
   */
-  if(apellido != '' || nombre != ''){
+  if(req.query.apellido != '' || req.query.nombre != ''){
     let queryAlumno = {}; //instancia de query para alumno
 
     //asinganción de query correspondiente
-    if(apellido != '')  
-      queryAlumno.apellido = {$regex : apellido, $options: 'i'};
-    if(nombre != '')
-      queryAlumno.nombre = {$regex : nombre, $options: 'i'}; 
+    if(req.query.apellido != '')  
+      queryAlumno.apellido = {$regex : req.query.apellido, $options: 'i'};
+    if(req.query.nombre != '')
+      queryAlumno.nombre = {$regex : req.query.nombre, $options: 'i'}; 
 
     let alumnos = await Alumno.find(queryAlumno);
     //console.log(alumnos);
@@ -45,25 +36,25 @@ cuotaCtrl.getCuotas = async (req, res) => {
   }
 
   /*
-  if(apellido != '')
-    criteriosAlu.apellido = {$regex : apellido, $options: 'i'};
-  if(nombre != '')
-    criteriosAlu.nombre = {$regex : nombre, $options: 'i'}; 
+  //Búsqueda por nombre o apellido
+  if(req.query.nombreApellido != '') {
+    let queryAlumno = {}; //instancia de query para alumno
 
-  //indico que me realice la búsqueda por las coincidencias de usuarios
-  if(criteriosAlu.apellido != undefined || criteriosAlu.nombre != undefined){
-    let alumnos = await Alumno.find(criteriosAlu);
+    queryAlumno.apellido = {$regex : req.query.nombreApellido, $optionas: i}
+    queryAlumno.nombre = {$regex : req.query.nombreApellido, $optionas: i}
+
+    let alumnos = await Alumno.find(queryAlumno);
     //console.log(alumnos);
-    let idAlumnos = [];
-    alumnos.forEach(element => {
+    let idAlumnos = []; //instancia de un array para almacenar id
+    alumnos.forEach(element => {  //añado los _id de los alumnos encontrados a un array
       idAlumnos.push(element._id)
     })
     //console.log(idAlumnos)
-    criterios.alumno = idAlumnos;
+    criterios.alumno = idAlumnos; //agrego el ide de los alumnos encontrados al query principal
   }
   */
   
-  console.log(criterios)
+  //console.log(criterios)
 
   //guardar resultado de la búsqueda
   let cuotas = await Cuota.find(criterios).populate('alumno');
