e7bf7a3317ef809e5cde3cb4e26fb020f05936ab
[George-Coria] <motoxjc10@gmail.com>
2021-06-19 03:34:08 -0300

    se agrega atributo state al modelo usuario

--- controllers/plan.controller.js
+++ controllers/plan.controller.js
@@ -2,60 +2,60 @@ const Plan = require('../models/plan');
 const planCtrl = {}
 
 planCtrl.getPlanes = async (req, res) => {
-      var planes = await Plan.find();
-      res.json(planes);
-  }
-  
+    var planes = await Plan.find();
+    res.json(planes);
+}
+
 planCtrl.addPlan = async (req, res) => {
-      var plan = new Plan(req.body);
-      try {
-          await plan.save();
-          res.json({
-              'status': '1',
-              'msg': 'Plan guardado.'
-          })
-      } catch (error) {
-          res.json({
-              'status': '0',
-              'msg': 'Error procesando operacion.'
-          })
-      }
-  }
-  
-  planCtrl.getPlan = async (req, res) => {
-      const plan = await Plan.findById(req.params.id);
-      res.json(plan);
-  }
-  
-  planCtrl.editPlan = async (req, res) => {
-      const vplan = new Plan(req.body);
-      try {
-          await Plan.updateOne({_id: req.body._id}, vplan);
-          res.json({
-              'status': '1',
-              'msg': 'Plan updated'
-          })
-      } catch (error) {
-      res.json({
-          'status': '0',
-          'msg': 'Error procesando la operacion'
-      })
-      }
-  }
-  
-  planCtrl.deletePlan = async (req, res)=>{
-   try {
-      await Plan.deleteOne({_id: req.params.id});
-      res.json({
-          status: '1',
-          msg: 'Plan removed'
-   })
-   } catch (error) {
-      res.json({
-          'status': '0',
-          'msg': 'Error procesando la operacion'
-      })
-   }
-  }
-  
-  module.exports = planCtrl;
+    var plan = new Plan(req.body);
+    try {
+        await plan.save();
+        res.json({
+            'status': '1',
+            'msg': 'Plan guardado.'
+        })
+    } catch (error) {
+        res.json({
+            'status': '0',
+            'msg': 'Error procesando operacion.'
+        })
+    }
+}
+
+planCtrl.getPlan = async (req, res) => {
+    const plan = await Plan.findById(req.params.id);
+    res.json(plan);
+}
+
+planCtrl.editPlan = async (req, res) => {
+    const vplan = new Plan(req.body);
+    try {
+        await Plan.updateOne({ _id: req.body._id }, vplan);
+        res.json({
+            'status': '1',
+            'msg': 'Plan updated'
+        })
+    } catch (error) {
+        res.json({
+            'status': '0',
+            'msg': 'Error procesando la operacion'
+        })
+    }
+}
+
+planCtrl.deletePlan = async (req, res) => {
+    try {
+        await Plan.deleteOne({ _id: req.params.id });
+        res.json({
+            status: '1',
+            msg: 'Plan removed'
+        })
+    } catch (error) {
+        res.json({
+            'status': '0',
+            'msg': 'Error procesando la operacion'
+        })
+    }
+}
+
+module.exports = planCtrl;
--- models/usuario.js
+++ models/usuario.js
@@ -4,6 +4,7 @@ const {Schema} = mongoose;
 const UsuarioSchema = new Schema({
     username: {type: String, required: true},
     password: {type: String, required:true},
+    state:{type: Boolean, required:true},
     rol: {type: String, required: true} //    -- ALUMNO / ENTRENADOR --
 });
 //exporto objeto para que pueda ser usado en otros lugares
