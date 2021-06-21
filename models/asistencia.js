b0c1378406952a0ccfe618681ddf2dde031189a2
Marcia Velarde <marciavelarde1991@gmail.com>
2021-06-21 17:53:19 -0300

    Se agrega required a atributo alumno del modelo asistencia

--- models/asistencia.js
+++ models/asistencia.js
@@ -4,7 +4,7 @@ const {Schema} = mongoose;
 const AsistenciaSchema = new Schema({
     
     fecha: {type: Date, required: true},
-    alumno: {type: Schema.Types.ObjectId,ref: alumno}   //usuario vinculado al alumno por id
+    alumno: {type: Schema.Types.ObjectId, ref: alumno, required: true}   //usuario vinculado al alumno por id
 });
 //exporto objeto para que pueda ser usado en otros lugares
 module.exports = mongoose.models.AsistenciaSchema || mongoose.model('Asistencia', AsistenciaSchema);
