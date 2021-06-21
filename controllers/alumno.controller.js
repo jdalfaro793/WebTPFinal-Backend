35c5e156cda8069226aebe95cdcdeedcc023e58f
Marcia Velarde <marciavelarde1991@gmail.com>
2021-06-21 18:29:51 -0300

    Se agrega modificaciones al metodo getAlumnos para filtrar por apellido o dni

--- controllers/alumno.controller.js
+++ controllers/alumno.controller.js
@@ -1,10 +1,16 @@
 const Alumno = require("../models/alumno");
 const alumnoCtrl = {};
 
-alumnoCtrl.getAlumnos= async (req, res) => {
-  var alumnos = await Alumno.find().populate("plan").populate("usuario");
+alumnoCtrl.getAlumnos = async (req, res) => {
+  let criterios = {};
+  if(req.query.apellido != '') 
+    criterios.apellido = { $regex: req.query.apellido, $options: "i" }
+  if(req.query.dni != '')
+    criterios.dni = { $regex: req.query.dni, $options: "i" }
+
+  let alumnos = await Alumno.find(criterios).populate("plan").populate("usuario");
   res.json(alumnos);
-}
+};
 
 alumnoCtrl.addAlumno = async (req, res) => {
   var alumno = new Alumno(req.body);
