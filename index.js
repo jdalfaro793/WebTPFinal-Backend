6e1b00e8f0170f79232b98793f022b970ab4b79b
Marcia Velarde <marciavelarde1991@gmail.com>
2021-06-22 02:12:46 -0300

    Se agrega las lineas necesarias al index.js para que soporte imagenes de mas tamaño

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var app = express();
 const {mongoose} = require('./database')
 const cors = require('cors');
 //middlewares
-app.use(express.json());
+app.use(express.json({limit: '25mb'}));
+app.use(express.urlencoded({limit: '25mb', extended: true }));
 app.use(cors({origin: 'http://localhost:4200'}));
 //Cargamos el modulo de direccionamiento de rutas para puntos
 app.use('/api/alumno', require('./routes/alumno.route'));
