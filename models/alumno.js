a8bfc9e5deb78e19296a8187dcf0702e92b75799
Marcia Velarde <marciavelarde1991@gmail.com>
2021-06-22 15:00:59 -0300

    Se agrega atributo a modelo Alumno

--- models/alumno.js
+++ models/alumno.js
@@ -15,8 +15,8 @@ const AlumnoSchema = new Schema({
     email: {type: String, required: true},
     fecha_inicio: {type: Date, required: true},
     plan: {type: Schema.Types.ObjectId, ref:plan, required:true},  //objeto de tipo plan , debe estar creado previamente
-    usuario: {type: Schema.Types.ObjectId,ref: usuario}   //usuario vinculado al alumno por id
-
+    usuario: {type: Schema.Types.ObjectId,ref: usuario},   //usuario vinculado al alumno por id
+    mes: {type: Number, required: true}
 
 });
 
